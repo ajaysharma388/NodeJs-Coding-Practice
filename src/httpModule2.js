b6af0e21edadca916961e4ab7622bf79639cc9af
AJAY SHARMA <ajaysharma2201@gmail.com>
2019-08-21 14:31:48 +0530

    update-2019.08.21.2

--- src/httpModule2.js
+++ src/httpModule2.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((req, res) => {
-     const readStream = fs.createReadStream('./Aj.jpg');
+     const readStream = fs.createReadStream('../text_files/Aj.jpg');
      res.writeHead(200, { 'Content-type': 'image/jpg' });
      readStream.pipe(res);
 }).listen(3000);
